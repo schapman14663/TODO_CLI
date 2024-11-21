ab84e0a284b400aef4e537e5059941e657eadc50
simon <schapman14663@gmail.com>
2024-11-21 21:59:28 +0000

    add priority, completion status and task deletion

--- src/TODO_CLI.py
+++ src/TODO_CLI.py
@@ -31,13 +31,37 @@ def main():
     # View Task Command
     view_parser = sub_parsers.add_parser("view", help="View the TODO list")
 
+    # Edit Task Commands
+    edit_parser = sub_parsers.add_parser("edit", help="edit task in TODO list")
+    edit_parser.add_argument("task_id", type=int, help="id of task to edit")
+    edit_parser.add_argument(
+        "-p",
+        "--priority",
+        choices=["low", "medium", "high"],
+        help="the updated priority of the TODO item",
+    )
+    edit_parser.add_argument(
+        "-c",
+        "--completion",
+        choices=["not started", "in progress", "done"],
+        help="the updated completion status of the TODO item",
+    )
+
+    # Delete Task Command
+    delete_parser = sub_parsers.add_parser("delete", help="delete task from TODO list")
+    delete_parser.add_argument("task_id", type=int, help="id of task to be deleted")
+
     args = parser.parse_args()
     print(args)
 
     if args.command == "add":
         add_task(args)
-    elif args.command == "view":
+    if args.command == "view":
         view_tasks()
+    if args.command == "edit":
+        edit_task(args)
+    if args.command == "delete":
+        delete_task(args)
 
 
 def add_task(args):
@@ -66,6 +90,24 @@ def save_task(tasks):
         json.dump(tasks, file)
 
 
+def edit_task(args):
+    update_task_priority(args)
+    update_task_completion(args)
+
+
+def delete_task(args):
+    tasks = read_tasks()
+    task_id = args.task_id
+
+    for task in tasks:
+        if task_id == task["id"]:
+            tasks.remove(task)
+            save_task(tasks)
+            print(f"TASK {task_id} DELETED")
+            return
+    print(f"{task_id} NOT FOUND")
+
+
 def read_tasks():
     try:
         with open(TODO_FILE, "r") as file:
@@ -77,9 +119,41 @@ def read_tasks():
 
 def view_tasks():
     tasks = read_tasks()
-    print("TODO List:")
+    print("| task id | task | priority | completion status |")
     for task in tasks:
-        print(f"Task {task['id']} : {task['task']}")
+        print(
+            f"| {task['id']} | {task['task']} | {task['priority']} | {task['completion']}"
+        )
+
+
+def update_task_priority(args):
+    print(args)
+    tasks = read_tasks()
+    task_id = args.task_id
+    new_priority = args.priority
+
+    for task in tasks:
+        if task_id == task["id"]:
+            task["priority"] = new_priority
+            save_task(tasks)
+            print(f"task priority changed to {new_priority}")
+            return
+    print(f"task {task_id} not found")
+
+
+def update_task_completion(args):
+    print(args)
+    tasks = read_tasks()
+    task_id = args.task_id
+    new_completion = args.completion
+
+    for task in tasks:
+        if task_id == task["id"]:
+            task["completion"] = new_completion
+            save_task(tasks)
+            print(f"task priority changed to {new_completion}")
+            return
+    print(f"task {task_id} not found")
 
 
 if __name__ == "__main__":
